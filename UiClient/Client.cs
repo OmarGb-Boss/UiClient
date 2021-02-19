2c90aef9f3b67903bf05228067032766d80a1830
Omar gb <neo_e_xe@hotmail.com>
2021-02-19 04:10:05 -0600

    Agregar archivos de proyecto.

---
+++ UiClient/Client.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace UiClient
+{
+    public class Client
+    {
+
+        static HttpClient client = new HttpClient();
+        //Artista por genero
+        public List<Artist> GetArtistByGenre(string Genre)
+        {
+            List<Artist> lstArtist = new List<Artist>();
+            string messageError = string.Empty;
+            try
+            {
+                HttpResponseMessage wcfResponse = client.GetAsync(string.Concat("http://localhost:61510/QueryMusic.svc/artistByGenre/", Genre)).Result;
+                HttpContent stream = wcfResponse.Content;
+                var data = stream.ReadAsStringAsync();
+                lstArtist = JsonConvert.DeserializeObject<List<Artist>>(data.Result);
+            }
+            catch (Exception e)
+            {
+                messageError = e.Message;
+            }
+
+            return lstArtist;
+        }
+
+
+        //Album por artista
+        public List<Album> GetAlbumByArtist(string Artist)
+        {
+            List<Album> lstArtist = new List<Album>();
+            string messageError = string.Empty;
+            try
+            {
+                HttpResponseMessage wcfResponse = client.GetAsync(string.Concat("http://localhost:61510/QueryMusic.svc/albumByArtist/", Artist)).Result;
+                HttpContent stream = wcfResponse.Content;
+                var data = stream.ReadAsStringAsync();
+                lstArtist = JsonConvert.DeserializeObject<List<Album>>(data.Result);
+
+            }
+            catch (Exception e) { messageError = e.Message; }
+
+            return lstArtist;
+        }
+
+        //Canciones por album
+        public List<Tracks> GetSongsByAlbum(string Album)
+        {
+            List<Tracks> lstAlbum = new List<Tracks>();
+            string messageError = string.Empty;
+            try
+            {
+                HttpResponseMessage wcfResponse = client.GetAsync(string.Concat("http://localhost:61510/QueryMusic.svc/songsByAlbum/", Album)).Result;
+                HttpContent stream = wcfResponse.Content;
+                var data = stream.ReadAsStringAsync();
+                lstAlbum = JsonConvert.DeserializeObject<List<Tracks>>(data.Result);
+
+            }
+            catch (Exception e) { messageError = e.Message; }
+
+            return lstAlbum;
+        }
+
+        //artistas por cancion
+        public List<Artist> GetArtistBySong(string song)
+        {
+            List<Artist> lstArtist = new List<Artist>();
+            string messageError = string.Empty;
+            try
+            {
+                HttpResponseMessage wcfResponse = client.GetAsync(string.Concat("http://localhost:61510/QueryMusic.svc/artistBySong/", song)).Result;
+                HttpContent stream = wcfResponse.Content;
+                var data = stream.ReadAsStringAsync();
+                lstArtist = JsonConvert.DeserializeObject<List<Artist>>(data.Result);
+            }
+            catch (Exception e) { messageError = e.Message; }
+            return lstArtist;
+        }
+
+        //All generes
+        public List<Genre> GetAllGenre()
+        {
+            List<Genre> lstGeneres = new List<Genre>();
+            string messageError = string.Empty;
+            try
+            {
+                HttpResponseMessage wcfResponse = client.GetAsync("http://localhost:61510/QueryMusic.svc/allGenre").Result;
+                HttpContent stream = wcfResponse.Content;
+                var data = stream.ReadAsStringAsync();
+                lstGeneres = JsonConvert.DeserializeObject<List<Genre>>(data.Result);
+            }
+            catch (Exception e) { messageError = e.Message; }
+            return lstGeneres;
+        }
+    }
+
+
+
+    public class Artist
+    {
+        public string NameArtist { get; set; }
+    }
+
+    public class Album
+    {
+        public string NameAlbum { get; set; }
+    }
+
+    public class Tracks
+    {
+        public string nameSong { get; set; }
+    }
+
+    public class SubGenre
+    {
+        public string NombreSubGenero { get; set; }
+    }
+    public class Genre
+    {
+        public string NameGenere { get; set; }
+        public List<SubGenre> lstSubGeneres;
+    }
+
+}
+
---
+++ UiClient/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+
+namespace UiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client cliente = new Client();
+
+
+            string Menu = "                ********Bienvenido******** \n Selecciona una opción \n 1) Consulta todos los generos musicales \n 2) Consulta artistas por genero         \n 3) Consulta albums por artista          \n 4) Consulta canciones por album         \n 5) Consulta artistas por nombre de una canción";
+
+            Console.WriteLine(Menu);
+
+            switch (Console.Read())
+            {
+                case '1':
+                    Console.Write("\n           Todos los generos \n");
+                    Console.Write("\n           Procesando . . . \n");
+                    ShowSpinner();
+                    foreach (var i in cliente.GetAllGenre())
+                    {
+                        Console.WriteLine("                                     ---------" + i.NameGenere + "---------");
+                        foreach (var j in i.lstSubGeneres)
+                        {
+                            Console.WriteLine(string.Concat("                   ", j.NombreSubGenero));
+                        }
+                        Console.WriteLine("                                     ------------------");
+                    }
+                    break;
+                case '2':
+                    Console.WriteLine("escribe un genero musical");
+                    Console.ReadLine();
+                    string gen;                                     
+                    gen = Console.ReadLine();
+                    Console.Write("\n           Procesando . . . \n \n \n");
+                    ShowSpinner();
+                    foreach (var i in cliente.GetAlbumByArtist(gen))
+                    {
+                        Console.WriteLine("---------" + i.NameAlbum + "---------");
+                    }
+
+                    break;
+                case '3':
+                    Console.WriteLine("escribe el nombre de un artista");
+                    Console.ReadLine();
+                    string artista;
+                    artista = Console.ReadLine();
+                    Console.Write("\n           Procesando . . . \n \n \n");
+                    ShowSpinner();
+                    foreach (var i in cliente.GetArtistByGenre(artista))
+                    {
+                        Console.WriteLine("---------" + i.NameArtist + "---------");
+                    }
+                    break;
+                case '4':
+                    Console.Write("Escribe un album ");
+                    Console.ReadLine();
+                    string album;
+                    album = Console.ReadLine();
+                    Console.Write("\n           Procesando . . . \n \n \n");
+                    ShowSpinner();
+                    foreach (var i in cliente.GetSongsByAlbum(album))
+                    {
+                        Console.WriteLine("---------" + i.nameSong + "---------");
+                    }
+                    break;
+                case '5':
+                    Console.Write("Escribe el nombre de un canción");
+                    Console.ReadLine();
+                    string Artist;
+                    Artist = Console.ReadLine();
+                    Console.Write("\n           Procesando . . . \n \n \n");
+                    ShowSpinner();
+                    foreach (var i in cliente.GetArtistBySong(Artist))
+                    {
+                        Console.WriteLine("---------" + i.NameArtist + "---------");
+                    }
+                    break;
+            }
+        }
+        static void ShowSpinner()
+        {
+            var counter = 0;
+            for (int i = 0; i < 50; i++)
+            {
+                switch (counter % 4)
+                {
+                    case 0: Console.Write("/"); break;
+                    case 1: Console.Write("-"); break;
+                    case 2: Console.Write("\\"); break;
+                    case 3: Console.Write("|"); break;
+                }
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                counter++;
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
